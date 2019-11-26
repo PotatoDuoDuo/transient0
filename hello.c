7439f45c8be705cf5c6cd7e91544aea9280f072b
hejialing0 <55731234+hejialing0@users.noreply.github.com>
2019-11-26 14:54:21 +0800

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-#include<curl/curl.h>
+#include<curl/cur.h>
 
 int main(void)
 {
@@ -8,14 +8,11 @@ int main(void)
   
   curl=curl_easy_init();
   if(curl){
-    
- curl_easy_setopt(curl,CURLOPT_URL,"ht
- tp://myip.ipip.net/");
+    curl_easy_setopt(curl,CURLOPT_URL,"http://myip.ipip.net/");
     res=curl_easy_perform(curl);
     if(res!=CURLE_OK)
-      fprintf(stderr,"curl_easy_perform()
-  failed:%s\n",
-         curl_easy_strerror(res));
+      fprintf(stderr,"curl_easy_perform() failed:%s\n",
+            curl_easy_strerror(res));
       curl_easy_cleanup(curl);
   }
   return 0;
